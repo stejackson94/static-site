566abbe544396a973b13181453a557121f5f0da3
stejackson94 <stephenjackson.career@outlook.com>
2025-06-25 11:55:24 +0100

    updating terraform for personal site and adding a gitignore

--- personal-site/infrastructure/main.tf
+++ personal-site/infrastructure/main.tf
@@ -1,145 +1,103 @@
 #Create S3 bucket to store static content in
 resource "aws_s3_bucket" "websiteBucket" {
-  bucket = var.domainName
-
+  bucket              = "stejackson.com"
+  bucket_prefix       = null
+  force_destroy       = null
+  object_lock_enabled = false
   tags = {
-    Name        = "website Bucket"
+    Name        = "website s3 bucket"
     BuiltBy     = "Terraform"
   }
 }
 
-resource "aws_s3_bucket_acl" "websiteBucket_acl" {
-  bucket = aws_s3_bucket.websiteBucket.id
-  acl    = "private"
-}
-
-locals {
-  s3_origin_id = "myS3Origin"
-}
-
-#Cloudfront Distribution
+# #Cloudfront Distribution
 resource "aws_cloudfront_distribution" "website_distribution" {
-  origin {
-    domain_name              = aws_s3_bucket.websiteBucket.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.website_access_control.id
-    origin_id                = local.s3_origin_id
+  aliases                         = [var.domainName]
+  comment                         = null
+  continuous_deployment_policy_id = null
+  default_root_object             = "index.html"
+  enabled                         = true
+  http_version                    = "http2"
+  is_ipv6_enabled                 = false
+  price_class                     = "PriceClass_All"
+  retain_on_delete                = false
+  staging                         = false
+  tags = {
+    Name        = "website cloudfront distribution"
+    BuiltBy     = "Terraform"
   }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
-  logging_config {
-    include_cookies = false
-    bucket          = "websitelogs.s3.amazonaws.com"
-  }
-  
-
-  aliases = ["stejackson.com"]
-
+  wait_for_deployment             = true
+  web_acl_id                      = null
   default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
+    allowed_methods            = ["GET", "HEAD"]
+    cache_policy_id            = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    cached_methods             = ["GET", "HEAD"]
+    compress                   = true
+    default_ttl                = 0
+    field_level_encryption_id  = null
+    max_ttl                    = 0
+    min_ttl                    = 0
+    origin_request_policy_id   = null
+    realtime_log_config_arn    = null
+    response_headers_policy_id = null
+    smooth_streaming           = false
+    target_origin_id           = aws_s3_bucket.websiteBucket.id
+    trusted_key_groups         = []
+    trusted_signers            = []
+    viewer_protocol_policy     = "allow-all"
+    grpc_config {
+      enabled = false
     }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
   }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
+  origin {
+    connection_attempts      = 3
+    connection_timeout       = 10
+    domain_name              = aws_s3_bucket.websiteBucket.id
+    origin_access_control_id = null
+    origin_id                = aws_s3_bucket.websiteBucket.id
+    origin_path              = null
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols     = ["TLSv1.3", "TLSv1.2"]
     }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
   }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  price_class = "PriceClass_200"
-
   restrictions {
     geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
+      locations        = []
+      restriction_type = "none"
     }
   }
-
-  tags = {
-    Name        = "website CF distribution"
-    BuiltBy     = "Terraform"
-  }
-
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.website_cert
+    acm_certificate_arn            = aws_acm_certificate.website_cert.arn
+    cloudfront_default_certificate = false
+    iam_certificate_id             = null
+    minimum_protocol_version       = "TLSv1.2_2021"
+    ssl_support_method             = "sni-only"
   }
+  depends_on = [ aws_acm_certificate.website_cert ]
 }
 
-resource "aws_cloudfront_origin_access_control" "website_access_control" {
-  name                              = "website_access_control"
-  description                       = "website access control policy"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-
-#ACM Cert
+# #ACM Cert
 resource "aws_acm_certificate" "website_cert" {
-  domain_name       = var.domainName
-  validation_method = "DNS"
-  provider = aws.virginia 
-
+  provider                  = aws.virginia
+  certificate_authority_arn = null
+  certificate_body          = null
+  certificate_chain         = null
+  domain_name               = var.domainName
+  early_renewal_duration    = null
+  key_algorithm             = "RSA_2048"
+  private_key               = null # sensitive
+  subject_alternative_names = [var.domainName]
   tags = {
-    Name        = "website cert"
+    Name        = "website ACM cert"
     BuiltBy     = "Terraform"
   }
-
-  lifecycle {
-    create_before_destroy = true
+  validation_method         = "DNS"
+  options {
+    certificate_transparency_logging_preference = "ENABLED"
   }
 }
--- personal-site/infrastructure/providers.tf
+++ personal-site/infrastructure/providers.tf
@@ -1,11 +1,21 @@
 terraform {
+  required_version = ">= 1.12.1"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+      configuration_aliases = [ aws.virginia ]
     }
   }
+
+  backend "s3" {
+    bucket = "personalwebsitestate"
+    key    = "state/terraform.tfstate"
+    region = "eu-west-2"
+  }
 }
+
 
 # Configure the AWS Provider
 provider "aws" {
@@ -17,3 +27,4 @@ provider "aws" {
   region = "us-east-1"
   alias = "virginia"
 }
+
